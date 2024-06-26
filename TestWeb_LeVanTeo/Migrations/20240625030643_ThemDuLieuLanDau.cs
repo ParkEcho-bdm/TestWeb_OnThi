66e95b1c67c145d3f15add30ddec4ab0ab688a8c
ParkEcho-bdm <buim8445@gmail.com>
2024-06-26 23:07:51 +0700

    Add project files.

---
+++ TestWeb_LeVanTeo/Migrations/20240625030643_ThemDuLieuLanDau.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestWeb_LeVanTeo.Migrations
+{
+    public partial class ThemDuLieuLanDau : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Price", "Quantity", "Title" },
+                values: new object[,]
+                {
+                    { 1, "A", 12000.0, 10, "PHP" },
+                    { 2, "A", 12000.0, 15, "C#" },
+                    { 3, "A", 12000.0, 10, "Quản trị SQL Server" },
+                    { 4, "A", 12000.0, 15, "Lập trình Node.js" },
+                    { 5, "A", 12000.0, 10, "ASP.Net Core MVC" },
+                    { 6, "A", 12000.0, 15, "Cấu trúc dữ liệu" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
---
+++ TestWeb_LeVanTeo/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWeb_LeVanTeo.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [StringLength(250)]
+        [Required(ErrorMessage ="Chưa nhập tiêu đề")]
+        public string Title { get; set; }
+        [StringLength(50)]
+        [Required]
+        public string Author { get; set; }
+        [Range(0,double.MaxValue, ErrorMessage ="Giá không hợp lệ")]
+        public double Price { get; set; }
+        [Range(1, 1000, ErrorMessage ="Số lượng không hợp lệ")]
+        public int Quantity { get; set; }
+    }
+}
